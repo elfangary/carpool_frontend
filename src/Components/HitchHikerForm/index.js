66014696cef601d398106d6678c007da51df057b
brent <A.ELFangary91@yahoo.com>
2017-12-28 19:55:48 +0200

    add_hhform_feature_v.2

--- src/Components/HitchHikerForm/index.js
+++ src/Components/HitchHikerForm/index.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import Locations from '../../Containers/LocationsContainer';
 import Days from '../Days';
-//import TimeFrame from '../TimeFrame';
 
 export default class HHForm extends Component {
     constructor(props){
@@ -14,33 +13,30 @@ export default class HHForm extends Component {
         };
     };
 
-    componentWillMount(){
-        this.props.getFilteredTrips();
-    }
+
 
     handleChange = (event) => {
-		this.setState({
-            day: this.day.value,
-            location_id: this.name.location_id.value,
-            start_time: this.name.start_time.value,
-            end_time: this.name.end_time.value
-        });
+        console.log(event.target.value);
+        const newState = {...this.state};
+        newState[event.target.name] = event.target.value;
+        this.setState(newState);
     };
 
     render(){
-        const { locations, trips } = this.props;
+        const { locations, trips, onChange , getFilteredTrips} = this.props;
         const { day, location_id, start_time, end_time } = this.state;
         return(
             <div>
                 <div>
-                    <Locations location_id={location_id} onChange={this.handleChange.bind(this)}/>
+                    <Locations onChange={this.handleChange.bind(this)}/>
                     <label>
                         Time Frame
                         <input type="time" name="start_time" value={start_time} onChange={this.handleChange}/>
                         <p>To</p>
                         <input type="time" name="end_time" value={end_time} onChange={this.handleChange}/>
                     </label>
-                    <Days day={day} onClick={this.handleChange}/>
+                    <Days onClick={this.handleChange.bind(this)}/>
+                    <input type="submit" onClick={() => this.props.getFilteredTrips(day, location_id, start_time, end_time)} />
                 </div>
                 <div>
                     {trips.map((trip) => {
@@ -52,6 +48,7 @@ export default class HHForm extends Component {
                         )
                     })}
                 </div>
+        
             </div>
         )
     }
